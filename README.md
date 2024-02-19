d6e4fe14ac4af5b605793bc97d3d76dfe715fc04
mansichug-h <mansichugh1714>
2024-02-19 12:45:50 +0530

    bug seen

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ making calc function
 add other function of calculator
 
 functions added as per the review
+
+some bugs please check!!
--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 #calculator
-def mul(a,b):
+def mul(a,b)
     return a*b
-def add(a,b):
+def add(a,b)
     return a+b
 def sub(a,b):
     return a-b
