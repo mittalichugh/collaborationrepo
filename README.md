77fbe0b8039a93ed77287a817f896d6c898a8370
mittali <mittali chugh>
2024-02-19 12:42:07 +0530

    third comit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 making calc function 
 
 add other function of calculator
+
+functions added as per the review
--- calculator.py
+++ calculator.py
@@ -1,3 +1,9 @@
 #calculator
 def mul(a,b):
     return a*b
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def div(a,b):
+    return a//b
